f32b90aed4e3cc1e0d5617954df940aec4e6a7fc
pervinn <pervinagayev28@gmail.com>
2024-04-27 18:13:34 +0400

    somtehing added

--- script.js
+++ script.js
@@ -11,22 +11,28 @@ let temp = 300;
 pipes.forEach(pipe => {
     pipe.style.left = 1600 - temp + "px";
     temp += 300;
-    pipe.style.marginBottom = (Math.floor(Math.random() * 5) + 1) * 100 + "px";
+    pipe.style.bottom = (Math.floor(Math.random() * 4)) * 100 + "px";
+    // pipe.style.marginBottom = (Math.floor(Math.random() * 4)) * 100 + "px";
 });
 
 function movePipes() {
     if (parseInt(document.getElementById('flyBtn').style.bottom <= 0)) {
         clearInterval(intervalId);
     }
+
     let pipeSpeed = 1.2;
     pipes.forEach(pipe => {
+        
+
+        console.log("asdad: "+document.getElementById('flyBtn').style.left);
+
         const pipeLeft = parseInt(pipe.style.left);
         pipe.style.left = (pipeLeft - pipeSpeed) + "px";
         if (pipeLeft + pipe.offsetWidth < 0) {
             pipe.style.left = "1400px";
             const randomHeight = Math.floor(Math.random() * 4) + 1;
             // pipe.style.height = randomHeight + '00px';
-            pipe.style.height = '300px';
+            pipe.style.height = '200px';
         }
     });
 };
@@ -34,16 +40,18 @@ function movePipes() {
 function moveBird() {
     document.getElementById('flyBtn').style.bottom = isNaN(document.getElementById('flyBtn').style.bottom) ? "0px"
         : (document.getElementById('flyBtn').style.bottom - 0.1) + "px";
+    document.getElementById('airPlane').style.transform = "rotate(45deg)";
+    if (isNaN(parseInt(document.getElementById('flyBtn').style.bottom)))
+        document.getElementById('airPlane').style.transform = "rotate(0deg)";
+
+    // clearInterval(intervalId);
+
 };
 
 window.onload = function () {
     intervalId = setInterval(movePipes, 0);
     setInterval(moveBird, 700);
 };
-// document.getElementById('flyBtn').addEventListener('click',function(){
-//     let bottom = parseInt(this.style.bottom);
-//     this.style.bottom = isNaN(bottom) ? "0px" : (bottom + 50) + "px";
-// });
 
 
 document.getElementById('game-container').addEventListener('click', function () {
@@ -57,9 +65,15 @@ document.getElementById('game-container').addEventListener('click', function () 
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowUp') {
+        document.getElementById('airPlane').style.transform = "rotate(-45deg)";
         let bottom = parseInt(document.getElementById('flyBtn').style.bottom);
         document.getElementById('flyBtn').style.bottom = isNaN(bottom) ? "50px" : (bottom + 300) + "px";
         if (bottom + 300 > 650)
             document.getElementById('flyBtn').style.bottom = "650px";
     }
+    if (event.key === "ArrowRight")
+        document.getElementById('airPlane').style.transform = "rotate(360deg)";
+    if (event.key === "ArrowLeft")
+        document.getElementById('airPlane').style.transform = "rotate(-360deg)";
+
 });
